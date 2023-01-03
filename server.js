9f20985a4d2dec2c0d85f065b83d1749f3b5484f
patrick chao <batsu@patricks-MacBook-Air.local>
2023-01-02 21:08:02 -0800

    started mongoose and added multer

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const multer = require("multer")
+const mongoose = require("mongoose")
+
 const express = require("express")
 const app = express()
+
+const upload = multer({ dest: "uploads" })
+
+mongoose.connect(process.env.DATABASE_URI)
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'));
@@ -8,4 +15,8 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.listen(3000)
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.send("hi")
+})
+
+app.listen(process.env.PORT)
